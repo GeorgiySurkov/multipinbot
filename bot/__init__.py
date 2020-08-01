2fb444bf4c01d39057df42d10440da1e4ab8e342
Georgiy Surkov <georgiy.surkov@gmail.com>
2020-08-01 21:45:08 +0500

    added config for webhook deployment.

--- bot/__init__.py
+++ bot/__init__.py
@@ -19,10 +19,15 @@ def run():
 
     try:
         loop.run_until_complete(db.init(Config))
-        executor.start_polling(dp, skip_updates=True)
+        if Config.DEPLOY:
+            executor.start_webhook(dp, webhook_path=Config.WEBHOOK_PATH,
+                                   host=Config.WEBAPP_HOST, port=Config.WEBAPP_PORT)
+        else:
+            executor.start_polling(dp, skip_updates=True)
     finally:
         # Graceful shutdown
         loop.run_until_complete(db.shutdown())
 
 
 from . import handlers
+from . import error_handlers
--- bot/config.py
+++ bot/config.py
@@ -6,9 +6,9 @@ class Config:
     TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
     DEPLOY = os.getenv('DEPLOY', 0) == 1
 
-    WEBHOOK_HOST = 'https://deploy-heroku-bot.herokuapp.com'  # name your app
-    WEBHOOK_PATH = '/webhook/'
+    WEBHOOK_HOST = os.getenv('WEBHOOK_HOST', 'https://deploy-heroku-bot.herokuapp.com')  # name your app
+    WEBHOOK_PATH = os.getenv('WEBHOOK_PATH', '/webhook/')
     WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
     WEBAPP_HOST = '0.0.0.0'
-    WEBAPP_PORT = os.environ.get('PORT')
+    WEBAPP_PORT = os.getenv('PORT')

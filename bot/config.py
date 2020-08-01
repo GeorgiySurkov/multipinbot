844cb34affbb2b3edcb76372d28e5dae90b6107c
Georgiy Surkov <georgiy.surkov@gmail.com>
2020-08-01 22:17:52 +0500

    no more heroku

--- bot/config.py
+++ bot/config.py
@@ -6,8 +6,8 @@ class Config:
     TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
     DEPLOY = os.getenv('DEPLOY', 0) == 1
 
-    WEBHOOK_HOST = os.getenv('WEBHOOK_HOST', 'https://deploy-heroku-bot.herokuapp.com')  # name your app
-    WEBHOOK_PATH = os.getenv('WEBHOOK_PATH', '/webhook/')
+    WEBHOOK_HOST = os.getenv('WEBHOOK_HOST')  # name your app
+    WEBHOOK_PATH = os.getenv('WEBHOOK_PATH')
     WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
     WEBAPP_HOST = '0.0.0.0'
